9f824ffe8eedef15c9889f11374f4056d06a261f
Viniccius Wirz <vinimovel23@gmail.com>
2023-08-31 14:27:45 -0300

    feat: get & get:id credentials

--- src/credentials/credentials.repository.ts
+++ src/credentials/credentials.repository.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
-import Cryptr from 'cryptr';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateCredentialDto } from './dto/create-credential.dto';
 
 @Injectable()
 export class CredentialsRepository {
-  private cryptr: Cryptr;
-  constructor(private readonly prisma: PrismaService) {
-    const Cryptr = require('cryptr');
-    this.cryptr = new Cryptr(process.env.CRYPTR_SECRET, {
-      pbkdf2Iterations: 10000,
-      saltLength: 10,
-    });
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
   create(body: CreateCredentialDto, user: User) {
-    const { password } = body;
     return this.prisma.credential.create({
       data: {
         ...body,
-        password: this.cryptr.encrypt(password),
         User: { connect: user },
       },
     });
@@ -30,5 +20,13 @@ export class CredentialsRepository {
     return this.prisma.credential.findUnique({
       where: { title_userId: { title: body.title, userId: userId } },
     });
+  }
+
+  findAllFromUser(user: User) {
+    return this.prisma.credential.findMany({ where: { User: user } });
+  }
+
+  findOne(id: number) {
+    return this.prisma.credential.findUnique({ where: { id } });
   }
 }
--- src/credentials/credentials.service.ts
+++ src/credentials/credentials.service.ts
@@ -1,30 +1,58 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import Cryptr from 'cryptr';
 import { User } from '@prisma/client';
 import { CredentialsRepository } from './credentials.repository';
 import { CreateCredentialDto } from './dto/create-credential.dto';
 
 @Injectable()
 export class CredentialsService {
-  constructor(private readonly repository: CredentialsRepository) {}
+  cryptr: Cryptr;
+
+  constructor(private readonly repository: CredentialsRepository) {
+    const Cryptr = require('cryptr');
+    this.cryptr = new Cryptr(process.env.CRYPTR_SECRET, {
+      pbkdf2Iterations: 10000,
+      saltLength: 10,
+    });
+  }
 
   async create(body: CreateCredentialDto, user: User) {
     await this.findWithTitle(body, user.id);
+    body.password = this.cryptr.encrypt(body.password);
     return await this.repository.create(body, user);
   }
 
-  findAll(user: User) {
-    return `This action returns all credentials`;
+  async findAll(user: User) {
+    const credentials = await this.repository.findAllFromUser(user);
+
+    return credentials.map(({ password, ...credential }) => {
+      return { ...credential, password: this.cryptr.decrypt(password) };
+    });
   }
 
-  findOne(id: number, user: User) {
-    return `This action returns a #${id} credential`;
+  async findOne(id: number, user: User) {
+    const { id: userId } = user;
+    const credential = await this.repository.findOne(id);
+    if (!credential) throw new NotFoundException("Credential doesn't exist.");
+    if (credential.userId !== userId)
+      throw new ForbiddenException("Credential doesn't belong to user.");
+
+    return {
+      ...credential,
+      password: this.cryptr.decrypt(credential.password),
+    };
   }
 
   remove(id: number, user: User) {
     return `This action removes a #${id} credential`;
   }
 
-  async findWithTitle(body: CreateCredentialDto, userId: number) {
+  private async findWithTitle(body: CreateCredentialDto, userId: number) {
     const credential = await this.repository.findWithTitle(body, userId);
     if (credential) throw new ConflictException('Title already registered');
   }
