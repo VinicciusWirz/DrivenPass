1dc3f9a4a0e88550665ad4218f7c1a5033cfe7ab
Viniccius Wirz <vinimovel23@gmail.com>
2023-09-01 20:00:43 -0300

    test: auth unit test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,98 @@
+import * as bcrypt from 'bcrypt';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
+import { UsersRepository } from '../users/users.repository';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signUpDto';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let prisma = new PrismaService();
+  let repository: UsersRepository;
+  let dto = new SignUpDto();
+  const SALT = 10;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      imports: [
+        JwtModule.register({
+          secret: process.env.JWT_SECRET,
+        }),
+      ],
+      providers: [AuthService, PrismaService, UsersService, UsersRepository],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(prisma)
+      .compile();
+
+    dto.email = 'email@email.com';
+    dto.password = 'Str0nG!P4szwuRd';
 
     service = module.get<AuthService>(AuthService);
+    repository = module.get<UsersRepository>(UsersRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('Sign-up service test', () => {
+    it('should return sucessfull message', async () => {
+      jest.spyOn(repository, 'findOneByEmail').mockResolvedValueOnce(null);
+      jest.spyOn(repository, 'create').mockResolvedValueOnce(null);
+
+      expect(await service.signUp(dto)).toEqual({
+        message: 'User created.',
+      });
+    });
+
+    it('should return throw Conflict error if user exist', () => {
+      jest.spyOn(repository, 'findOneByEmail').mockResolvedValueOnce({
+        id: 1,
+        email: dto.email,
+        password: dto.password,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      expect(service.signUp(dto)).rejects.toThrow(new ConflictException());
+    });
+  });
+
+  describe('Sign-in service test', () => {
+    it('should return sucessfull message', async () => {
+      jest.spyOn(repository, 'findOneByEmail').mockResolvedValueOnce({
+        id: 1,
+        email: dto.email,
+        password: bcrypt.hashSync(dto.password, SALT),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      expect(await service.signIn(dto)).toEqual({
+        token: expect.any(String),
+      });
+    });
+
+    it("should return throw Unauthorized error if user doesn't exist", () => {
+      jest.spyOn(repository, 'findOneByEmail').mockResolvedValueOnce(null);
+
+      expect(service.signIn(dto)).rejects.toThrow(
+        new UnauthorizedException(`Email or password not valid.`),
+      );
+    });
+
+    it("should return throw Unauthorized error if password doesn't match", () => {
+      jest.spyOn(repository, 'findOneByEmail').mockResolvedValueOnce({
+        id: 1,
+        email: dto.email,
+        password: bcrypt.hashSync('123', SALT),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      expect(service.signIn(dto)).rejects.toThrow(
+        new UnauthorizedException(`Email or password not valid.`),
+      );
+    });
   });
 });
